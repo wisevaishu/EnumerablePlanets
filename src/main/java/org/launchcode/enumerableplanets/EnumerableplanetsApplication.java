521b5cbe52d5da5345718a5e3680ba682823c1a0
John Woolbright <jwoolbright23@gmail.com>
2023-07-31 14:08:58 -0500

    starter code for enumerableplanets

---
+++ src/main/java/org/launchcode/enumerableplanets/EnumerableplanetsApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.enumerableplanets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnumerableplanetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnumerableplanetsApplication.class, args);
+	}
+
+}
