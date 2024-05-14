78b96ecdd8cc22223ca5738920206575f552bb0a
wisevaishu <vpm.win@gmail.com>
2024-05-13 20:41:28 -0500

    enum exercise completed

--- src/main/java/org/launchcode/controllers/PlanetController.java
+++ src/main/java/org/launchcode/controllers/PlanetController.java
@@ -1,13 +1,18 @@
 package org.launchcode.controllers;
 
+import org.launchcode.Data.Planets;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class PlanetController {
 
-    @GetMapping
-    public String displayIndex() {
+
+
+    @GetMapping("/")
+    public String displayIndex(Model model) {
+        model.addAttribute("planets", Planets.values());
         return "index";
     }
 }
